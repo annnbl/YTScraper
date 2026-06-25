9d2957f9ab147d6e3c95e4ba68078b2042fd703c
Ann <annnoble14@gmail.com>
2026-06-26 00:10:24 +0530

    Update gemini version

--- analysis_reddit.py
+++ analysis_reddit.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 
 def analyze_reddit(keyword, posts):
